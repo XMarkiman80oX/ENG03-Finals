36d5d6dd5bbb64bbd68ab5cbc111bf7c874978ed
Rylan Lim <77736504+FurehXD@users.noreply.github.com>
2025-05-13 09:05:45 +0800

    initial commit

---
+++ DX3D/Source/DX3D/Graphics/RenderSystem.cpp
@@ -0,0 +1,69 @@
+/*MIT License
+
+C++ 3D Game Tutorial Series (https://github.com/PardCode/CPP-3D-Game-Tutorial-Series)
+
+Copyright (c) 2019-2025, PardCode
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.*/
+
+#include <DX3D/Graphics/RenderSystem.h>
+#include <DX3D/Graphics/GraphicsLogUtils.h>
+#include <DX3D/Graphics/SwapChain.h>
+
+using namespace dx3d;
+
+
+dx3d::RenderSystem::RenderSystem(const RenderSystemDesc& desc): Base(desc.base)
+{
+	D3D_FEATURE_LEVEL featureLevel{};
+	UINT createDeviceFlags{};
+
+#ifdef _DEBUG
+	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+	DX3DGraphicsLogErrorAndThrow(D3D11CreateDevice(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, createDeviceFlags,
+		NULL, 0, D3D11_SDK_VERSION,
+		&m_d3dDevice, &featureLevel, &m_d3dContext),
+		"Direct3D11 initialization failed.");
+
+	DX3DGraphicsLogErrorAndThrow(m_d3dDevice->QueryInterface(IID_PPV_ARGS(&m_dxgiDevice)),
+		"QueryInterface failed to retrieve IDXGIDevice.");
+
+	DX3DGraphicsLogErrorAndThrow(m_dxgiDevice->GetParent(IID_PPV_ARGS(&m_dxgiAdapter)),
+		"GetParent failed to retrieve IDXGIAdapter.");
+
+	DX3DGraphicsLogErrorAndThrow(m_dxgiAdapter->GetParent(IID_PPV_ARGS(&m_dxgiFactory)),
+		"GetParent failed to retrieve IDXGIFactory.");
+
+}
+
+dx3d::RenderSystem::~RenderSystem()
+{
+}
+
+SwapChainPtr dx3d::RenderSystem::createSwapChain(const SwapChainDesc& desc) const
+{
+	return std::make_shared<SwapChain>(desc, getGraphicsResourceDesc());
+}
+
+GraphicsResourceDesc dx3d::RenderSystem::getGraphicsResourceDesc() const noexcept
+{
+	return { {m_logger}, shared_from_this(), *m_d3dDevice.Get(), *m_dxgiFactory.Get() };
+}
