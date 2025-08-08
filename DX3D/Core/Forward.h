25331941cdd6eb1ab1c3558d48d5161935e50aac
XMarkiman80oX <115690499+XMarkiman80oX@users.noreply.github.com>
2025-08-09 05:18:05 +0800

    reordered forward declarations

--- DX3D/Core/Forward.h
+++ DX3D/Core/Forward.h
@@ -1,36 +1,42 @@
 #pragma once
-#include <stdexcept>
 #include <memory>
+#include <stdexcept>
 
 namespace dx3d
 {
-    class Core;
-    class Logger;
-    class Game;
+    // Game Objects
+    class AGameObject;
+    class CameraObject;
+    class Cube;
+    class Plane;
 
+    // Core Systems
+    class Core;
+    class Game;
+    class Logger;
+    class SelectionSystem;
+    class ViewportManager;
+
+    // Graphics
     class GraphicsEngine;
     class RenderSystem;
     class SwapChain;
     class Display;
+    class Window;
 
+    // Buffers
     class VertexBuffer;
     class IndexBuffer;
     class ConstantBuffer;
     class DepthBuffer;
     class RenderTexture;
-    class Window;
 
-    class AGameObject;
-    class CameraObject;
-    class SelectionSystem;
-    class ViewportManager;
-    class Cube;
-    class Plane;
-
+    // Type Aliases
     using i32 = int;
     using ui32 = unsigned int;
     using f32 = float;
     using d64 = double;
 
+    // Pointer Alias
     using SwapChainPtr = std::shared_ptr<SwapChain>;
 }
